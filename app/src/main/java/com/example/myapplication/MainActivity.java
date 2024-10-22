71b379925f74a948772e257aa85884557327479f
RZRexton <ooiruizhe@gmail.com>
2024-10-22 23:35:05 +0800

    Completed first cover trial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.logo);
+        Animation logoAnimation = AnimationUtils.loadAnimation(this, R.anim.logoanimation);
+
+        imageView.startAnimation(logoAnimation);
     }
 }
